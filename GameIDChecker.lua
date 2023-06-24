03011c31d1d094647a3402454e5a705d836ac4c2
Cerbiale <136471759+Cerbiale@users.noreply.github.com>
2023-06-24 17:18:39 +0200

    Create GameIDChecker.lua

---
+++ GameIDChecker.lua
@@ -0,0 +1,11 @@
+if game.PlaceId == 662417684 then   -- Lucky Block Battlegrounds
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Cerbiale/ProjectOmega/main/LuckyBlockBattlegrounds.lua"))()
+elseif game.PlaceId == 3101667897 then   -- Legends Of Speed
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Cerbiale/ProjectOmega/main/LegendsOfSpeed.lua"))()
+elseif game.PlaceId == 189707 then -- Natural Disaster Survival
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Cerbiale/ProjectOmega/main/NaturalDisasterSurvival.lua"))()
+elseif game.PlaceId == 3956818381 then -- Ninja Legends
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Cerbiale/ProjectOmega/main/NinjaLegends.lua"))()
+elseif game.PlaceId == 13230182363 then -- +1 Money Every Click
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Cerbiale/ProjectOmega/main/PlusOneMoney.lua"))()
+end
