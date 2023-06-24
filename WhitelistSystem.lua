2a35aed8c84ee87327b21533de9b539090e1384a
Cerbiale <136471759+Cerbiale@users.noreply.github.com>
2023-06-24 17:21:39 +0200

    Update WhitelistSystem.lua

--- WhitelistSystem.lua
+++ WhitelistSystem.lua
@@ -220,17 +220,7 @@ if Player.UserId ~= 1 then
 	tween5:Play()
 	wait(2)
 	WhitelistSystem.Enabled = false
-	if game.PlaceId == 662417684 then   -- Lucky Block Battlegrounds
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Cerbiale/ProjectOmega/main/LuckyBlockBattlegrounds.lua"))()
-	elseif game.PlaceId == 3101667897 then   -- Legends Of Speed
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Cerbiale/ProjectOmega/main/LegendsOfSpeed.lua"))()
-	elseif game.PlaceId == 189707 then -- Natural Disaster Survival
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/Cerbiale/ProjectOmega/main/NaturalDisasterSurvival.lua"))()
-	elseif game.PlaceId == 3956818381 then -- Ninja Legends
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/Cerbiale/ProjectOmega/main/NinjaLegends.lua"))()
-	elseif game.PlaceId == 13230182363 then -- +1 Money Every Click
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/Cerbiale/ProjectOmega/main/PlusOneMoney.lua"))()
-	end
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Cerbiale/ProjectOmega/main/GameIDChecker.lua"))()
 	game.CoreGui:FindFirstChild("WhitelistSystem"):Destroy()
 else
 	wait(2)
